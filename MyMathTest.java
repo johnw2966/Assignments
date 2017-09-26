42ffd910f5cf777bffea82dd0da89e0f3bf15429
John Walsh <lt00036644@students.ittralee.local>
2017-09-26 14:59:48 +0100

    Initial commit

---
+++ MyMathTest.java
@@ -0,0 +1,24 @@
+package com.jw.assigns;
+
+import javax.swing.*;
+
+
+public class MyMathTest {
+    public static void main(String[] args){
+        //declare and populate two variables
+        int a=Integer.parseInt(JOptionPane.showInputDialog("Enter number"));
+        int b=Integer.parseInt(JOptionPane.showInputDialog("Enter number"));
+
+        //call MyMath method
+        MyMath.sum(a,b);
+
+        //create object of MyMath class
+        MyMath c = new MyMath(Integer.parseInt(JOptionPane.showInputDialog("Enter number")));
+
+        //call different??? MyMath method
+        int d= c.sum(a);
+
+        JOptionPane.showMessageDialog(null,"Sum is: "+d);
+
+    }
+}
